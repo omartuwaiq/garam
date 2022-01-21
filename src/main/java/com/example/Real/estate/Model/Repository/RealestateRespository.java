025a9449d96b6f6addcb08a133bede71fe11d0b6
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-22 01:33:13 +0300

    Delete src directory

--- src/main/java/com/example/Real/estate/Model/Repository/RealestateRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Real.estate.Model.Repository;
-
-import com.example.Real.estate.Model.Entities.Realestate;
-import com.example.Real.estate.Model.Entities.Users;
-import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RealestateRespository extends JpaRepository<Realestate,Integer> {
-}
